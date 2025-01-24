7c9ebeaa21e5cd9bfd98f666cc655d295d479be9
Amit Garg <gargamit.online@gmail.com>
2025-01-25 09:53:07 +1100

    sample code to Get Session Id

--- src/main/java/com/nijamrit/SpringBootWebPlay/controller/HomeController.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 //@Controller // Returns Page Name
 @RestController // Return Data
 public class HomeController {
@@ -15,5 +17,10 @@ public class HomeController {
     // @ResponseBody // Either use RestController on Class OR use ResponseBody on Method
     public String welcome() {
         return "Namaste !!";
+    }
+
+    @RequestMapping("/session")
+    public String checkSessionId(HttpServletRequest request) {
+        return "Session Id: " + request.getSession().getId();
     }
 }
