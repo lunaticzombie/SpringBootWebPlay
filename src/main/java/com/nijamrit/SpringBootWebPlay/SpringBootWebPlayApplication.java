64641c5377831e2f501d2bae91635aa7057ced2a
Amit Garg <gargamit.online@gmail.com>
2025-01-25 09:23:25 +1100

    Tried Spring Boot Starter Security

--- src/main/java/com/nijamrit/SpringBootWebPlay/SpringBootWebPlayApplication.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/SpringBootWebPlayApplication.java
@@ -10,7 +10,11 @@ public class SpringBootWebPlayApplication {
 		// Note: Starter Console Project - No dependencies
 		// Note: Web Starter Project - Spring Web, Spring Boot Dev Tools
 		// Note: for DB Layer - Spring Boot Starter Data JPA, H2 Database Engine (for quick setup DB)
-				// Update pom and application.properties and u r good to go
+			// Update pom and application.properties and u r good to go
+		// Note: for Security login - Spring Boot Starter Security, user + generated security password on console
+			// can also set in properties file spring.security.user.name/password
+			// /logout is the page to logout
+			// can also send Auth in Postman
 		// Then create a Repository layer by creating an interface that extends JpaRepository<Product, Integer>, Product is th Class used for schema and second arg is Primary key
 		// Then in Product add @Entity and @Id, This would create a table in h2 DB
 		SpringApplication.run(SpringBootWebPlayApplication.class, args);
