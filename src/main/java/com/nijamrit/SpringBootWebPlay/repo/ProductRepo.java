ad89df89a99db0cf17b5623a2e551926da0f11de
Amit Garg <gargamit.online@gmail.com>
2025-01-25 07:59:36 +1100

    added JPQL sample

--- src/main/java/com/nijamrit/SpringBootWebPlay/repo/ProductRepo.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/repo/ProductRepo.java
@@ -1,11 +1,18 @@
 package com.nijamrit.SpringBootWebPlay.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nijamrit.SpringBootWebPlay.model.Product;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer>{
-
+    // Search with JPQL - JPA Query Language, uses Class and fields instead of table and column
+    // @Query("query on keyword")
+    // Using OR AND LOWER etc we can create complex queries
+    @Query("SELECT p from Product p WHERE p.prodName LIKE CONCAT('%', :keyword, '%')")
+    List<Product> searchProducts(String keyword);
 }
