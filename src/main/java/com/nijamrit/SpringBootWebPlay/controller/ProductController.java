788ccbe83ba0b0e45ae07f5a1404d11ed3e62e4f
Amit Garg <gargamit.online@gmail.com>
2025-01-25 07:23:57 +1100

    updated for Autogeneration of Product Id

--- src/main/java/com/nijamrit/SpringBootWebPlay/controller/ProductController.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/controller/ProductController.java
@@ -43,6 +43,7 @@ public class ProductController {
     public Product addProduct(@RequestBody Product product) {
         System.out.println("Received product (to Add) on Product Controller: " + product);
         // Note: multiple products when not given id, are unable to save more than 1 product because of primary key conflict
+        // Resolved by using @ for Autogeneration of Id in Product Class
         return service.addProduct(product);
     }
 }
--- src/main/java/com/nijamrit/SpringBootWebPlay/model/Product.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/model/Product.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Component
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int prodId;
     private String prodName;
     private int price;
