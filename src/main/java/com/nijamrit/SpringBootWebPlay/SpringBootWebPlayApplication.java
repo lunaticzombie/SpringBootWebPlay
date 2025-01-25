246bd20e649329cca438a2d59d8b02229755b017
Amit Garg <gargamit.online@gmail.com>
2025-01-25 21:10:23 +1100

    added info about creating custom Configuration Class for custom defined Security, basically to use authentication details from DB

--- src/main/java/com/nijamrit/SpringBootWebPlay/SpringBootWebPlayApplication.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/SpringBootWebPlayApplication.java
@@ -18,6 +18,7 @@ public class SpringBootWebPlayApplication {
 			// CSRF (Cross Site Resourse Forgery) - third party gaining access using ur session
 			// either create a new session everytime, or create CSRF token that can be used multiple times
 			// name=_csrf type="hidden" value=<token> (Note: it is required when sending update add delete type of request)
+			// create config class (@Configuration + @EnableWebSecurity) and define @Bean in it to inject object, to create your own filter chain of security
 		// Then create a Repository layer by creating an interface that extends JpaRepository<Product, Integer>, Product is th Class used for schema and second arg is Primary key
 		// Then in Product add @Entity and @Id, This would create a table in h2 DB
 		SpringApplication.run(SpringBootWebPlayApplication.class, args);
