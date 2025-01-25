34633ba138d4c0204bc236cd7f6bf3332bbccb40
Amit Garg <gargamit.online@gmail.com>
2025-01-25 12:41:05 +1100

    added info about CSRF

--- src/main/java/com/nijamrit/SpringBootWebPlay/SpringBootWebPlayApplication.java
+++ src/main/java/com/nijamrit/SpringBootWebPlay/SpringBootWebPlayApplication.java
@@ -14,7 +14,10 @@ public class SpringBootWebPlayApplication {
 		// Note: for Security login - Spring Boot Starter Security, user + generated security password on console
 			// can also set in properties file spring.security.user.name/password
 			// /logout is the page to logout
-			// can also send Auth in Postman
+			// can also send Autherization (Basic Auth) in Postman
+			// CSRF (Cross Site Resourse Forgery) - third party gaining access using ur session
+			// either create a new session everytime, or create CSRF token that can be used multiple times
+			// name=_csrf type="hidden" value=<token> (Note: it is required when sending update add delete type of request)
 		// Then create a Repository layer by creating an interface that extends JpaRepository<Product, Integer>, Product is th Class used for schema and second arg is Primary key
 		// Then in Product add @Entity and @Id, This would create a table in h2 DB
 		SpringApplication.run(SpringBootWebPlayApplication.class, args);
